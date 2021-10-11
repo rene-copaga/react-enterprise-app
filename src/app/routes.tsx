9c4de868a93c4022ccb046aa03d068751a2f8e11
Rene Copaga <rene.copaga@gmail.com>
2021-10-10 23:24:35 -0400

    -Navigating React Router: <Redirect >

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import AboutPage from './views/pages/AboutPage';
 import Home from './views/pages/Home';
 import NotFoundPage from './views/pages/NotFoundPage';
@@ -10,6 +10,7 @@ const Routes = () => {
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={AboutPage} />
       <Route exact path="/not-found" component={NotFoundPage} />
+      <Redirect exact from={'*'} to={'/not-found'} />
     </Switch>
   );
 }
