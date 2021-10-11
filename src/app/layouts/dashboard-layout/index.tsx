09403958f5f6744b2421f1050e09a0f21d506c2e
Rene Copaga <rene.copaga@gmail.com>
2021-10-11 07:45:45 -0400

    -Navigating Path: <Link> -React Hook: useRouteMatch

---
+++ src/app/layouts/dashboard-layout/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Grid } from '@material-ui/core'
+import DashboardSidebarNavigation from './dashboard-sidebar-navigation'
+
+type Props = {
+  children: React.ReactNode;
+}
+
+const Dashboard = ({ children }: Props) => {
+  return (
+    <Grid
+      container
+      direction="row"
+      justifyContent="flex-start"
+      alignItems="flex-start"
+    >
+      <DashboardSidebarNavigation /> {children}
+    </Grid>
+  )
+}
+
+export default Dashboard;
