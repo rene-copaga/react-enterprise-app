c4ef5bbed001d41c45aa4cb67917624dd4b17af0
Rene Copaga <rene.copaga@gmail.com>
2021-10-10 23:19:35 -0400

    --Adding Navigation Links

--- src/app/components/navigation-bar.tsx
+++ src/app/components/navigation-bar.tsx
@@ -6,6 +6,8 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import { Link } from 'react-router-dom';
+import { colors } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -14,6 +16,10 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     menuButton: {
       marginRight: theme.spacing(2),
+    },
+    link: {
+      color: colors.lightBlue[50],
+      textDecoration: 'none',
     },
     title: {
       flexGrow: 1,
@@ -28,13 +34,29 @@ export default function NavigationBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <Link className={`${classes.link} ${classes.title}`} to={'/'}>
+            LOGO
+          </Link>
+          <Button color="inherit">
+            <Link className={classes.link} to='/'>
+              Home
+            </Link>
+          </Button>
+          <Button color="inherit">
+            <Link className={classes.link} to='/about'>
+              About
+            </Link>
+          </Button>
+          <Button color="inherit">
+            <Link className={classes.link} to='/dashboard'>
+              Dashboard
+            </Link>
+          </Button>
+          <Button color="inherit">
+            <Link className={classes.link} to='/login'>
+              Login
+            </Link>
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -2,12 +2,14 @@ import React, { lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import AboutPage from './views/pages/AboutPage';
 import Home from './views/pages/Home';
+import NotFoundPage from './views/pages/NotFoundPage';
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={AboutPage} />
+      <Route exact path="/not-found" component={NotFoundPage} />
     </Switch>
   );
 }
