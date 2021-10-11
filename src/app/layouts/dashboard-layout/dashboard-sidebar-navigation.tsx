4b8b783925dee8e41ccc9474e0bae72d077345f9
Rene Copaga <rene.copaga@gmail.com>
2021-10-10 23:47:29 -0400

    Creating the Dashboard Layout

---
+++ src/app/layouts/dashboard-layout/dashboard-sidebar-navigation.tsx
@@ -0,0 +1,6 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Toolbar from '@material-ui/core/Toolbar';
+import { useRouteMatch } from 'react-router';
