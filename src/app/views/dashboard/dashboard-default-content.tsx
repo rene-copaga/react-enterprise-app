9a8b4fefe8434f0b8c429c2b0f1adb50fc758079
Rene Copaga <rene.copaga@gmail.com>
2021-10-18 07:18:48 -0400

    -Making Requests with Axios 2

--- src/app/views/dashboard/dashboard-default-content.tsx
+++ src/app/views/dashboard/dashboard-default-content.tsx
@@ -1,17 +1,26 @@
-import React, { useEffect } from 'react'
+import { SaleType } from 'models/sale-type';
+import React, { useEffect, useState } from 'react'
 import { getSalesAxios } from 'services/saleService';
 
 const DashboardDefaultContent = () => {
+  const [sales, setSales] = useState<SaleType[]>([]);
+
+  const fetchSales = async () => {
+    const { data } = await getSalesAxios();
+    console.log(data); // ← to check in the console if we are successfully getting the data
+    setSales(data);
+  }
 
   useEffect(() => {
   //code to run after render goes here
 
-    getSalesAxios();
+    fetchSales();
   }, []); //&#x00DF; empty array means to ‘run once’
 
   return (
     <div>
       <h1>Dashboard Default Content</h1>
+      <h2>{sales.length}</h2>
     </div>
   );
 };
