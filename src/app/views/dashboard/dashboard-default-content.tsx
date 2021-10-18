62af524d738a75af30a7f5c66d9aa77d739729e5
Rene Copaga <rene.copaga@gmail.com>
2021-10-18 09:00:35 -0400

    -Making Requests with Axios 3

--- src/app/views/dashboard/dashboard-default-content.tsx
+++ src/app/views/dashboard/dashboard-default-content.tsx
@@ -1,21 +1,104 @@
 import { SaleType } from 'models/sale-type';
 import React, { useEffect, useState } from 'react'
 import { getSalesAxios } from 'services/saleService';
+import { Theme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core';
+import { useTheme } from 'styled-components';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    minHeight: '100%',
+  }
+}));
+
+const getChartStyling = (theme: Theme) => ({
+  chart: {
+    background: theme.palette.background.paper,
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: ['#13affe', '#fbab49'],
+  dataLabels: {
+    enabled: false,
+  },
+  grid: {
+    borderColor: theme.palette.divider,
+    yaxis: {
+      lines: {
+        show: false,
+      },
+    },
+  },
+  legend: {
+    show: true,
+    labels: {
+      colors: theme.palette.text.secondary,
+    },
+  },
+  plotOptions: {
+    bar: {
+      columnWidth: '40%',
+    },
+  },
+  stroke: {
+    show: true,
+    width: 2,
+    colors: ['transparent'],
+  },
+  theme: {
+    mode: theme.palette.type,
+  },
+  tooltip: {
+    theme: theme.palette.type,
+  },
+  xaxis: {
+    axisBorder: {
+      show: true,
+      color: theme.palette.divider,
+    },
+    axisTicks: {
+      show: true,
+      color: theme.palette.divider,
+    },
+    categories: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    labels: {
+      style: {
+        colors: theme.palette.text.secondary,
+      },
+    },
+  },
+  yaxis: {
+    axisBorder: {
+      show: true,
+      color: theme.palette.divider,
+    },
+    axisTicks: {
+      show: true,
+      color: theme.palette.divider,
+    },
+    labels: {
+      style: {
+        colors: theme.palette.text.secondary,
+      },
+    },
+  },
+})
 
 const DashboardDefaultContent = () => {
+  const classes = useStyles();
+  const theme = useTheme();
+
   const [sales, setSales] = useState<SaleType[]>([]);
 
   const fetchSales = async () => {
     const { data } = await getSalesAxios();
-    console.log(data); // ← to check in the console if we are successfully getting the data
     setSales(data);
   }
 
   useEffect(() => {
-  //code to run after render goes here
-
     fetchSales();
-  }, []); //&#x00DF; empty array means to ‘run once’
+  }, []);
 
   return (
     <div>
