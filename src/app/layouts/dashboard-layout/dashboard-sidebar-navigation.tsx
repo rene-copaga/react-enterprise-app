311778a7ebb954df1c08381c39b4ee4b07a9c746
Rene Copaga <rene.copaga@gmail.com>
2022-05-22 12:41:41 -0400

    Updating the Sidebar Dashboard

--- src/app/layouts/dashboard-layout/dashboard-sidebar-navigation.tsx
+++ src/app/layouts/dashboard-layout/dashboard-sidebar-navigation.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
@@ -7,7 +7,13 @@ import { useRouteMatch } from 'react-router';
 import { List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import SettingsIcon from '@material-ui/icons/Settings';
-import { PieChart as PieChartIcon } from 'react-feather';
+import {PieChart as PieChartIcon,
+  ShoppingCart as ShoppingCartIcon,
+  ChevronUp as ChevronUpIcon,
+  ChevronDown as ChevronDownIcon,
+  List as ListIcon,
+  FilePlus as FilePlusIcon,
+  LogOut as LogOutIcon,} from 'react-feather';
 import { Divider, ListSubheader } from '@material-ui/core';
 
 const drawerWidth = 240;
@@ -45,7 +51,13 @@ const DashboardSidebarNavigation = () => {
   const classes = useStyles();
   const { url } = useRouteMatch();
 
+  const [open, setOpen] = useState(false);
+
   useEffect(() => {}, []);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
 
   return (
     <div className={classes.root}>
