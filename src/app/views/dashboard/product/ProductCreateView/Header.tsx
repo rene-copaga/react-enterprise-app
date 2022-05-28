04182a3d6b058c776857308703def540c8844d76
Rene Copaga <rene.copaga@gmail.com>
2022-05-28 11:19:05 -0400

    Updating the ProductCreateView

--- src/app/views/dashboard/product/ProductCreateView/Header.tsx
+++ src/app/views/dashboard/product/ProductCreateView/Header.tsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import clsx from 'clsx';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import {
+  Breadcrumbs,
+  Button,
+  Grid,
+  Link,
+  Typography,
+  makeStyles,
+  Box,
+} from '@material-ui/core';
 
-const Header = () => {
+type Props = {
+  className?: string;
+};
+
+const Header = ({ className, ...rest }: Props) => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Header - CreativeView Works!</h1>
-    </div>
-  )
-}
+    <Grid
+      className={clsx(classes.root, className)}
+      container
+      justify="space-between"
+      spacing={3}
+      {...rest}
+    >
+      <Grid item>
+        <Breadcrumbs
+          separator={<NavigateNextIcon fontSize="small" />}
+          aria-label="breadcrumb"
+        >
+          <Link
+            variant="body1"
+            color="inherit"
+            to="/dashboard"
+            component={RouterLink}
+          >
+            Dashboard
+          </Link>
+          <Box mb={3}>
+            <Typography variant="body1" color="inherit">
+              Create Product
+            </Typography>
+          </Box>
+        </Breadcrumbs>
+        <Typography variant="h4" color="textPrimary">
+          Create a new product
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Button component={RouterLink} to="/dashboard/list-products">
+          Cancel
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
+
+const useStyles = makeStyles(() => ({
+  root: {},
+}));
 
 export default Header;
